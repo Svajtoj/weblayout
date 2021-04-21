107ae00c9022e483725855168498f666d07b6081
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-04-21 12:48:57 +0300

    Выполнил задание по кроссбраузерности

---
+++ 15_cross-browser/js/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//burger
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const burger = document.querySelector('.burger');
+  const headerMenu = document.querySelector('.header__menu');
+
+  burger.onclick = () => {
+    burger.classList.toggle('burger--active');
+    headerMenu.classList.toggle('header__menu--active');
+  }
+
+  // Slider
+
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  // Tabs
+
+  const tabMenuBtnElems = document.querySelectorAll('.tab-menu__btn');
+  const tabContentElems = document.querySelectorAll('.tab-content');
+
+  tabMenuBtnElems.forEach(function (tabsBtn) {
+
+    tabsBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path;
+
+      tabMenuBtnElems.forEach(function (removeClassBtn) {
+        removeClassBtn.classList.remove('tab-menu__btn--active');
+      });
+
+      tabContentElems.forEach(function (removeClassContent) {
+        removeClassContent.classList.remove('tab-content--active');
+      });
+
+      document.querySelector(`[data-target="${path}"]`).classList.add('tab-content--active');
+      document.querySelector(`[data-path="${path}"]`).classList.add('tab-menu__btn--active');
+    });
+  });
+
+
+  $('.accordion').accordion({
+    heightStyle: 'content',
+    collapsible: true,
+    // header: '> .accordion-item > .accordion-header',
+    header: "h3",
+    active: true
+  });
+});
+
+
