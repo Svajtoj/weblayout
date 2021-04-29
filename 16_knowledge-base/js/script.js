584d6bdc6339212a7d53ef8f832bf0dfec1b7af0
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-04-29 22:03:33 +0300

    Добавил Яндекс карту на страницу

--- 16_knowledge-base/js/script.js
+++ 16_knowledge-base/js/script.js
@@ -9,4 +9,30 @@ document.addEventListener('DOMContentLoaded', () => {
     itemSelectText: ' ',
   });
 
+  ymaps.ready(init);
+
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+      center: [48.872185073737896, 2.3542149999999555],
+      zoom: 15
+    });
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: `Париж, ул. дю Фобур Сен Дени 54`,
+      balloonContent: 'Франция, Иль-де-Франс, Париж, X округ Парижа, улица дю Фобур Сен Дени 54'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/Subtract.svg',
+      // Размеры метки.
+      iconImageSize: [48, 48],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+    }),
+
+      myMap.geoObjects.add(myPlacemark);
+  }
 });
